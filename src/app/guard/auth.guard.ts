064648099b9469fdd05281a4e97a80c463cea39f
Yuto Yoneda <yoneda@local.bgp.ne.jp>
2020-07-05 05:12:12 +0900

    fixed guard function

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,15 +1,31 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
+import {Observable} from 'rxjs';
+import {map, take, tap} from "rxjs/operators";
+import {AngularFireAuth} from "@angular/fire/auth";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(
+    private router: Router,
+    private auth: AngularFireAuth,
+  ) {
+  }
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.auth.user.pipe(
+      take(1),
+      map(user => !!user),
+      tap(loggedIn => {
+        if (!loggedIn) {
+          this.router.navigate(['/']).then();
+        }
+      })
+    );
   }
-  
 }
