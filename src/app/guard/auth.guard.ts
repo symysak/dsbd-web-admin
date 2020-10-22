3063fea45b42753795e8972259a06c2370fe8c2e
yoneyan <github@yoneyan.net>
2020-10-22 15:40:22 +0900

    guardシステムの変更

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,31 +1,18 @@
 import {Injectable} from '@angular/core';
-import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs';
-import {map, take, tap} from "rxjs/operators";
-import {AngularFireAuth} from "@angular/fire/auth";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(
-    private router: Router,
-    private auth: AngularFireAuth,
-  ) {
+  constructor() {
   }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return this.auth.user.pipe(
-      take(1),
-      map(user => !!user),
-      tap(loggedIn => {
-        if (!loggedIn) {
-          this.router.navigate(['/']).then();
-        }
-      })
-    );
+    return null !== sessionStorage.getItem('AccessToken');
   }
 }
