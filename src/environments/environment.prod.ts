3ec05044888e0bd13f8e8d25079ec47263555329
yoneyan <contact@yoneyan.net>
2020-11-03 20:26:52 +0900

    production用のURLの変更

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,19 @@
 export const environment = {
   production: true,
   api: {
-    url: 'http://localhost:8081',
+    url: 'http://dashboard.local.bgp.ne.jp:8081',
     path: '/api/v1',
   },
   websocket: {
-    url: 'ws://localhost:8081',
+    url: 'ws://dashboard.local.bgp.ne.jp:8081',
     path: '/ws/v1'
   }
+  // api: {
+  //   url: 'http://localhost:8081',
+  //   path: '/api/v1',
+  // },
+  // websocket: {
+  //   url: 'ws://localhost:8081',
+  //   path: '/ws/v1'
+  // }
 };
