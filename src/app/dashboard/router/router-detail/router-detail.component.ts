7d88e46e74fb70f593694619e104d40cf5f6ad58
yoneyan <github@yoneyan.net>
2021-01-18 03:52:45 +0900

    [add] router detailページの完成

--- src/app/dashboard/router/router-detail/router-detail.component.ts
+++ src/app/dashboard/router/router-detail/router-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RouterService} from '../../../service/router.service';
+import {CommonService} from '../../../service/common.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-router-detail',
@@ -7,9 +11,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RouterDetailComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private routerService: RouterService,
+    private route: ActivatedRoute,
+    private commonService: CommonService,
+  ) {
   }
 
+  public routers: any;
+  public loading = true;
+  public id: string;
+  public routerInput = new FormGroup({
+    ID: new FormControl(),
+    noc: new FormControl(),
+    hostname: new FormControl(),
+    address: new FormControl(),
+    enable: new FormControl(),
+  });
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.routerService.get(this.id).then(response => {
+      console.log(response);
+      this.routerInput.patchValue({
+        ID: response.router[0].ID,
+        noc: response.router[0].noc,
+        enable: response.router[0].enable
+      });
+      if (response.status) {
+        this.routers = response.router[0];
+        this.loading = false;
+        this.commonService.openBar('OK', 5000);
+      } else {
+        console.log('error: ' + JSON.stringify(response));
+        return;
+      }
+    });
+  }
+
+  update(): void {
+    const json = JSON.stringify(this.routerInput.getRawValue());
+    console.log(json);
+    this.routerService.update(this.id, json).then(response => {
+      if (response.status) {
+        this.commonService.openBar('OK', 5000);
+        location.reload();
+      } else {
+        this.commonService.openBar('NG', 5000);
+        console.log('error: ' + JSON.stringify(response));
+      }
+    });
+  }
 }
