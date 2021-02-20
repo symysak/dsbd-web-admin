debea6b1b2d0658effe3d46d8ee23f0878779d89
yoneyan <github@yoneyan.net>
2021-02-20 21:31:06 +0900

    [fix] #16 Issueの修正

--- src/app/dashboard/support/chat/chat.component.ts
+++ src/app/dashboard/support/chat/chat.component.ts
@@ -66,5 +66,6 @@ export class ChatComponent implements OnInit, OnDestroy {
       message: this.comment.value
     };
     this.supportService.sendMessage(JSON.stringify(body));
+    this.comment.setValue('');
   }
 }
