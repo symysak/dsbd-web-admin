4d20609f07bdd3ad738a7853cb594e059757927f
yoneyan <github@yoneyan.net>
2021-03-17 08:07:52 +0900

    [support] stg環境への対応化

---
+++ src/environments/environment.stg.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  api: {
+    url: 'http://dsys-dev-osk.local.bgp.ne.jp:8081',
+    path: '/api/v1',
+  },
+  websocket: {
+    url: 'ws://dsys-dev-osk.local.bgp.ne.jp:8081',
+    path: '/ws/v1'
+  }
+};
