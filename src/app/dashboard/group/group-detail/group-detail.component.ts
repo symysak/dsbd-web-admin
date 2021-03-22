d70c9734824fbda6bf235fe7a67aa4e8401963a5
yoneyan <github@yoneyan.net>
2021-03-22 19:34:59 +0900

    [fix] #46

--- src/app/dashboard/group/group-detail/group-detail.component.ts
+++ src/app/dashboard/group/group-detail/group-detail.component.ts
@@ -447,7 +447,7 @@ export class GroupDetailCreateService implements OnInit {
     }
 
     if (this.needJPNIC || this.needGlobalAS) {
-      if (this.checkV4 && this.checkV6) {
+      if (!this.checkV4 && !this.checkV6) {
         this.commonService.openBar('v4とv6どちらか選択する必要があります。。', 5000);
         return;
       }
