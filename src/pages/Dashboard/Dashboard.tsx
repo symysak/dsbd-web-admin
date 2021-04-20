573c46f1897e2b7c2afebd0d7d8866459c62a5e6
Yuto Yoneda <contact@y-net.dev>
2021-04-21 05:49:47 +0900

    [add] add dashboard component

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DashboardComponent from "../../components/Dashboard/Dashboard";
+
+
+export default function Dashboard() {
+    // const dsbd = Dashboard({title: "test"})
+    // const Dashboard = (props: any) => {
+    return (
+        <DashboardComponent title="Dashboard">
+            {/*<h1>Group Info</h1>*/}
+        </DashboardComponent>
+    );
+}
