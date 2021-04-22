1235cc13df00ed8470b1a3006331b631a69ced6f
Yuto Yoneda <contact@y-net.dev>
2021-04-23 03:56:24 +0900

    [add] add put function

--- src/api/Group.ts
+++ src/api/Group.ts
@@ -1,5 +1,47 @@
 import axios from "axios";
 import {restfulApiConfig} from "./Config";
+import {GroupDetailData} from "../pages/Group/GroupDetail/interface";
+
+export function Put(id: number, data: GroupDetailData): Promise<{ error: string; data: any }> {
+    return axios.put(restfulApiConfig.apiURL + "/group/" + id, data, {
+        headers: {
+            'Content-Type': 'application/json',
+            ACCESS_TOKEN: sessionStorage.getItem('AccessToken'),
+        }
+    }).then(res => {
+        return {
+            error: "",
+            data: res.data.group
+        };
+    }).catch(err => {
+        console.log(err);
+        return {
+            error: err,
+            data: null
+        };
+    })
+}
+
+
+export function Get(id: string): Promise<{ error: string, data: any }> {
+    return axios.get(restfulApiConfig.apiURL + "/group/" + id, {
+        headers: {
+            'Content-Type': 'application/json',
+            ACCESS_TOKEN: sessionStorage.getItem('AccessToken'),
+        }
+    }).then(res => {
+        return {
+            error: "",
+            data: res.data.group
+        };
+    }).catch(err => {
+        console.log(err);
+        return {
+            error: err.error.error,
+            data: null
+        };
+    })
+}
 
 export function GetAll(): Promise<{ error: string, data: any }> {
     return axios.get(restfulApiConfig.apiURL + "/group", {
