b976d03366cd124fa92ae222287f54b3f7b402c5
yoneyan <github@yoneyan.net>
2021-05-15 23:13:25 +0900

    [fix] メッセージが重複する問題の修正

--- src/pages/Support/SupportDetail/SupportDetail.tsx
+++ src/pages/Support/SupportDetail/SupportDetail.tsx
@@ -34,7 +34,7 @@ export default function SupportDetail() {
             if (res.error === "") {
                 console.log(res.data);
                 setBaseChatData([]);
-                let tmpChat = baseChatData
+                let tmpChat = []
 
                 for (const tmp of res.data.chat) {
                     let userName = "管理者";
