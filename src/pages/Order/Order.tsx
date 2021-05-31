ecc777b035b3bfea208476605336d0b09a520dbc
yoneyan <github@yoneyan.net>
2021-06-01 04:54:14 +0900

    [add] added order page.

---
+++ src/pages/Order/Order.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from "../../components/Dashboard/Dashboard";
+
+
+export default function Order() {
+    // const dsbd = Dashboard({title: "test"})
+    // const Dashboard = (props: any) => {
+    return (
+        <Dashboard title="Order">
+            <h1>この機能は現在、未実装です</h1>
+        </Dashboard>
+    );
+}
