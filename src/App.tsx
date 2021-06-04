b2554e7fa6f9d3d90e9b7c5a5afae066204ffb26
yoneyan <github@yoneyan.net>
2021-06-05 01:57:41 +0900

    [fix] Remove some dead code

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.css';
 import SignIn from './pages/Login/SignIn';
-import SignUp from "./pages/SignUp";
-import PasswordRecovery from "./pages/PasswordRecovery";
 import Group from "./pages/Group/Group";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Notice from './pages/Notice/Notice';
