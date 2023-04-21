de418d96b74a3ba901d74b3b0a4fac5b083696f8
yoneyan <contact@yoneyan.net>
2023-04-22 01:29:58 +0900

    [support] Supported react18.

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { SnackbarProvider } from 'notistack'
 import { RecoilRoot } from 'recoil'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.render(
-  <React.StrictMode>
+const container = document.getElementById('root')
+if (container) {
+  const root = createRoot(container)
+  root.render(
     <RecoilRoot>
       <SnackbarProvider maxSnack={3}>
         <App />
       </SnackbarProvider>
     </RecoilRoot>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
-
+  )
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
