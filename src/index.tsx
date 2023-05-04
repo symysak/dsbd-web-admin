fa9a5830c260894c71a4ef76b79662deb81d925b
symysak <yoshiaki@the.suyamas.jp>
2023-05-04 23:41:15 +0900

    add close button to snackBar

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,26 @@ import React from 'react'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { SnackbarProvider } from 'notistack'
+import { SnackbarProvider, closeSnackbar } from 'notistack'
 import { RecoilRoot } from 'recoil'
 import { createRoot } from 'react-dom/client'
+import { IconButton } from "@mui/material"
+import CloseIcon from "@mui/icons-material/Close"
 
 const container = document.getElementById('root')
 if (container) {
   const root = createRoot(container)
   root.render(
     <RecoilRoot>
-      <SnackbarProvider maxSnack={3}>
+      <SnackbarProvider
+        maxSnack={3}
+        autoHideDuration={5000}
+        action={(snackbarId) => (
+          <IconButton onClick={() => closeSnackbar(snackbarId)}>
+            <CloseIcon />
+          </IconButton>
+        )}
+      >
         <App />
       </SnackbarProvider>
     </RecoilRoot>
